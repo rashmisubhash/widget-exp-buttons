45d08e1e77cf035e59b0b70221ef6364ba885b50
shiraazAh <shiraaz.ahammed@gmail.com>
2023-02-27 20:26:35 +0530

    A

--- src/component/PaymentButton.js
+++ src/component/PaymentButton.js
@@ -10,10 +10,6 @@ const PaymentButton = ({eventUrl}) => {
                 <>
                 <div style={{width: "100vw", height: "100vh", backgroundColor: "black", opacity: "0.6", position: "fixed", top: "0", left: "0"}}></div>
                 <div style={{border: "1px solid black", width: "400px", height: "70vh", position: "fixed", top: "50%", left: "50%", marginLeft: "-200px", marginTop: "-35vh", zIndex: "10"}}>
-                    {/* <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
-                    <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
-                    <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
-                <input type="text" style={{marginBottom: "0px", width: "100%"}}></input> */}
                     <iframe title="reg" src={`https://dev.konfhub.com/widget/${eventUrl}`} style={{width: "100%", height: "100%"}} />
                 </div>
                 </>
