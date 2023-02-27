b09accd216f995f3a6253d9ff3ff313a845d95e1
shiraazAh <shiraaz.ahammed@gmail.com>
2023-02-27 19:15:17 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import PaymentButton from "./component/PaymentButton"
 
 function App() {
+  
+  document.currentScript = document.currentScript || (function() {
+    var scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1];
+  })();
+
+  let eventUrl = document.currentScript.getAttribute('eventUrl');
+
+  console.log(eventUrl)
+
   return (
     <div className="">
-      <PaymentButton />
+      <PaymentButton eventUrl={eventUrl} />
     </div>
   );
 }
--- src/component/PaymentButton.js
+++ src/component/PaymentButton.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react"
 
-const PaymentButton = () => {
+const PaymentButton = ({eventUrl}) => {
     const [open, setOpen] = useState(false)
     return (
         <div>
             <button onClick={() => setOpen(!open)}>Pay</button>
             {open && (
-                <div style={{border: "1px solid black", padding: "20px", margin: "20px", width: "60%"}}>
+                <>
+                <div style={{width: "100vw", height: "100vh", backgroundColor: "black", opacity: "0.6", position: "fixed", top: "0", left: "0"}}></div>
+                <div style={{border: "1px solid black", width: "400px", height: "70vh", position: "fixed", top: "50%", left: "50%", marginLeft: "-200px", marginTop: "-35vh", zIndex: "10"}}>
+                    {/* <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
                     <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
                     <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
-                    <input type="text" style={{marginBottom: "10px", width: "100%"}}></input>
-                    <input type="text" style={{marginBottom: "0px", width: "100%"}}></input>
+                <input type="text" style={{marginBottom: "0px", width: "100%"}}></input> */}
+                    <iframe title="reg" src={`https://dev.konfhub.com/widget/jt-75dc36e9`} style={{width: "100%", height: "100%"}} />
                 </div>
+                </>
             )}
         </div>
 
