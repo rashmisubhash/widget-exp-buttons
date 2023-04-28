3fb27c6c3d5ceb8940fa83bd88ee01ff5c6dc243
rashmi1120 <rashmi.bsubash@gmail.com>
2023-04-28 07:28:04 +0530

    changes

--- src/component/PaymentButton.js
+++ src/component/PaymentButton.js
@@ -3,11 +3,7 @@ import axios from "axios";
 
 const PaymentButton = ({ eventUrl }) => {
   const [open, setOpen] = useState(false);
-  const [buttonText, setButtonText] = useState("");
-  const [buttonTheme, setButtonTheme] = useState("");
-  const [tickets, setTickets] = useState();
   const [screenSize, setScreenSize] = useState(null);
-  // const [eventUrl, setEventUrl] = useState("")
 
   const [eventDetails, setEventDetails] = useState(null);
   const [widgetLink, setWidgetLink] = useState(null);
@@ -22,22 +18,25 @@ const PaymentButton = ({ eventUrl }) => {
     setEventDetails(result);
     console.log(result);
     setWidgetLink(
-      `https://dev.konfhub.com/widget/${result.event_url
-      }?desc=${!result.hide_ticket_description}&tickets=${result.attached_ticket_ids.toString()}`
+      `https://dev.konfhub.com/widget/${
+        result.event_url
+      }?desc=${!result.hide_ticket_description}&tickets=${result.attached_ticket_ids.toString()}&btnBg=${eventDetails.brand_color.substring(
+        1
+      )}&tertiaryBg=${eventDetails.brand_color.substring(1)}`
     );
   }
 
   function checkMediaQuery() {
-    setScreenSize(window.screen.availWidth)
+    setScreenSize(window.screen.availWidth);
     // If the inner width of the window is greater then 768px
     if (window.innerWidth > 768) {
       // Then log this message to the console
-      console.log('Media Query Matched!')
+      console.log("Media Query Matched!");
     }
   }
 
   // Add a listener for when the window resizes
-    window.addEventListener('resize', checkMediaQuery);
+  window.addEventListener("resize", checkMediaQuery);
 
   useEffect(() => {
     // button id
@@ -46,32 +45,13 @@ const PaymentButton = ({ eventUrl }) => {
       : "dev_btn_607220c204ce";
     window.konfhubButton(button_id);
     if (button_id.includes("dev_btn")) getEventDetails(button_id);
-    setScreenSize(window.screen.availWidth)
-    // let buttonText = document.currentScript?.getAttribute("data-button_text")
-    //     ? document.currentScript.getAttribute("data-button_text")
-    //     : "Register"; // 1
-    // let buttonTheme = document.currentScript?.getAttribute("data-button_theme")
-    //     ? document.currentScript.getAttribute("data-button_theme")
-    //     : "#fb5850"; // 2
-    // // let buttonTheme = document.currentScript?.getAttribute('data-button_theme') ? document.currentScript.getAttribute('data-button_theme') : "#fb5850"; // 2
-    // let tickets = document.currentScript?.getAttribute("data-tickets")
-    //     ? document.currentScript.getAttribute("data-tickets")
-    //     : ""; // 2
-    // let tickets_desc = document.currentScript?.getAttribute("data-tickets_desc")
-    //     ? document.currentScript.getAttribute("data-tickets_desc")
-    //     : false; // 2
-
-    // setButtonText(buttonText);
-    // setButtonTheme(buttonTheme);
-
-    // setTickets(tickets);
-    // setDesc(tickets_desc);
-    // setEventUrl(eventUrl)
+    setScreenSize(window.screen.availWidth);
   }, []);
 
   return (
     eventDetails != null &&
-    widgetLink != null && screenSize != null && (
+    widgetLink != null &&
+    screenSize != null && (
       <div>
         <button
           style={{
@@ -86,10 +66,9 @@ const PaymentButton = ({ eventUrl }) => {
           }}
           onClick={() => setOpen(!open)}
         >
-          {eventDetails.button_name}
+          {eventDetails.button_name}ssss
         </button>
         {open && (
-
           <>
             <div
               style={{
@@ -102,9 +81,7 @@ const PaymentButton = ({ eventUrl }) => {
                 left: "0",
               }}
             ></div>
-            <div
-              className="modal-container"
-            >
+            <div className="modal-container">
               <button
                 style={{
                   position: "absolute",
@@ -119,7 +96,7 @@ const PaymentButton = ({ eventUrl }) => {
                   cursor: "pointer",
                   borderTopRightRadius: "10px",
                   borderBottomLeftRadius: "5px",
-                  padding: "2px 7px 4px"
+                  padding: "2px 7px 4px",
                 }}
                 onClick={() => setOpen(false)}
               >
@@ -137,132 +114,27 @@ const PaymentButton = ({ eventUrl }) => {
               />
             </div>
           </>
-          // window.innerWidth > 768 ?
-          //   <>
-          //     <div
-          //       style={{
-          //         width: "100vw",
-          //         height: "100vh",
-          //         backgroundColor: "black",
-          //         opacity: "0.6",
-          //         position: "fixed",
-          //         top: "0",
-          //         left: "0",
-          //       }}
-          //     ></div>
-          //     <div
-          //       className="modal-container" style={{
-          //         position: "absolute",
-          //         left: "50%",
-          //         top: "50%",
-          //         transform: "translate(-50%, -50%)",
-          //         border: "0px solid black",
-          //         width: "400px",
-          //         height: "70vh",
-          //         zIndex: "10",
-          //         boxShadow: "0px 0px 20px #383838",
-          //         borderRadius: "10px",
-          //         overflow: "hidden"
-          //       }}
-          //     >
-          //       <button
-          //         style={{
-          //           position: "absolute",
-          //           zIndex: "5",
-          //           right: "20px",
-          //           fontWeight: "100",
-          //           top: "16px",
-          //           fontSize: "18px",
-          //           color: "#fb5850",
-          //           border: "1px solid #fb5850",
-          //           cursor: "pointer",
-          //         }}
-          //         onClick={() => setOpen(false)}
-          //       >
-          //         X
-          //       </button>
-          //       <iframe
-          //         style={{
-          //           border: "none",
-          //           outline: "none",
-          //           width: "100%",
-          //           height: "100%",
-          //         }}
-          //         title="reg"
-          //         src={widgetLink}
-          //       />
-          //     </div>
-          //   </> : <>
-          //     <div
-          //       style={{
-          //         width: "100vw",
-          //         height: "100vh",
-          //         backgroundColor: "black",
-          //         opacity: "0.6",
-          //         position: "fixed",
-          //         top: "0",
-          //         left: "0",
-          //       }}
-          //     ></div>
-          //     <div
-          //       className="modal-container" style={{
-          //         position: "relative",
-          //         left: "0%",
-          //         top: "0%",
-          //         border: "0px solid black",
-          //         width: "100%",
-          //         height: "70vh",
-          //         zIndex: "10",
-          //         boxShadow: "0px 0px 20px #383838",
-          //         borderRadius: "10px",
-          //         overflow: "hidden"
-          //       }}
-          //     >
-          //       <button
-          //         style={{
-          //           position: "absolute",
-          //           zIndex: "5",
-          //           right: "20px",
-          //           fontWeight: "100",
-          //           top: "16px",
-          //           fontSize: "18px",
-          //           color: "#fb5850",
-          //           border: "1px solid #fb5850",
-          //           cursor: "pointer",
-          //         }}
-          //         onClick={() => setOpen(false)}
-          //       >
-          //         X
-          //       </button>
-          //       <iframe
-          //         style={{
-          //           border: "none",
-          //           outline: "none",
-          //           width: "100%",
-          //           height: "100%",
-          //         }}
-          //         title="reg"
-          //         src={widgetLink}
-          //       />
-          //     </div></>
         )}
 
-        <style jsx>{
-          `
+        <style jsx>{`
           .modal-container {
-            position: ${screenSize > 700 ? "absolute !important" : "relative !important"};
+            position: ${screenSize > 700
+              ? "absolute !important"
+              : "relative !important"};
             left: ${screenSize > 700 ? "50% !important" : "0% !important"};
             top: ${screenSize > 700 ? "50% !important" : "0% !important"};
-            transform: ${screenSize > 700 ? "translate(-50%, -50%) !important" : "translate(0px) !important"};
+            transform: ${screenSize > 700
+              ? "translate(-50%, -50%) !important"
+              : "translate(0px) !important"};
             border: 0px solid black !important;
-            width: ${screenSize > 700 ?"600px !important": "100% !important"};
+            width: ${screenSize > 700 ? "600px !important" : "100% !important"};
             height: 90vh !important;
             z-index: 10 !important;
             box-shadow: 0px 0px 20px #383838 !important;
             border-radius: 10px !important;
             overflow: hidden !important;
           }
-          
+
           @media screen and (max-width: 768px) {
             .modal-container {
               width: 100% !important;
@@ -272,8 +144,7 @@ const PaymentButton = ({ eventUrl }) => {
               top: 0% !important;
             }
           }
-          `
-        }</style>
+        `}</style>
       </div>
     )
   );
